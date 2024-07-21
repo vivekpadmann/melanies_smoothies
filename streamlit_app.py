76ef54f700f1dbd32cb685c328aac50ceeff4445
vivekpadmann <VIVEKPADMANN@GMAIL.COM>
2024-07-21 12:13:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
 
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options.search_on").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(

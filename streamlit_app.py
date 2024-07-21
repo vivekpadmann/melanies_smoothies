14a59c02f02539c74ef032a04c628881db3d1b30
vivekpadmann <VIVEKPADMANN@GMAIL.COM>
2024-07-21 11:33:17 -0500

    Update streamlit_app.py
    Import Requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,6 +50,11 @@ if ingredients_list:
         st.success('Your Smoothie is ordered'+', '+ name_on_order+'!', icon="✅")
 
 
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+
 
          
      

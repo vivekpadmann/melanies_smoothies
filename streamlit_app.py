a590ae0cfc73c97d99ac514d4afa6e69a75b633d
vivekpadmann <VIVEKPADMANN@GMAIL.COM>
2024-07-21 11:49:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,11 +14,8 @@ st.write(
     """
 )
 
-
-
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
-
 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -29,13 +26,12 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
     )
-if ingredients_list: 
+if ingredients_list:
+
     ingredients_string = ''
-    
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(fruit_chosen +' Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
@@ -52,11 +48,3 @@ if ingredients_list:
     if time_to_start:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered'+', '+ name_on_order+'!', icon="✅")
-
-
-
-
-
-
-         
-     
